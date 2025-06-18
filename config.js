94c746740d0596d4f8fa7397721eacf4b6c7f8c9
findus-jdk <findus.jdk@gmail.com>
2025-06-18 17:31:43 +0530

    addddd

---
+++ config.js
@@ -0,0 +1,14 @@
+const companyConfig = {
+    companyName: "VANDAN LINING WORKS",
+    slogan: "Innovating for a better tomorrow",
+    logoUrl: "logo.jpeg",
+    contact: {
+        phone: "+918490046650",
+    },
+    socialMedia: {
+        whatsapp: "https://wa.me/918490046650",
+        instagram: "https://www.instagram.com/vandanlining1234?igsh=MWl5YjM1eGhla2tvcQ==",
+        googleReviews: "https://www.google.com/search?sca_esv=c00251c694cd13aa&sxsrf=AE3TifMZuYciS-4X1MUEpevpCuJhARPNSA:1750245339926&si=AMgyJEtREmoPL4P1I5IDCfuA8gybfVI2d5Uj7QMwYCZHKDZ-EwiG8AMeEMcVmjH6rYC0EqEj5U9-O1SDUv0zu5MzaMlCZmlm3uUTTPTjN0Ld1xCmzMDeYwWuOCC79eEishhmipOqSsQGV0xoOaeoi20bn3rGBReG7w%3D%3D&q=Vandan+Learning+Works+Reviews&sa=X&ved=2ahUKEwiF_IX-6_qNAxXayzgGHQzgCLYQ0bkNegQIGxAD&biw=1600&bih=792&dpr=1.8#lrd=0x395f5a87119e43f3:0x39f779eed0000000,3,,,,",
+        map: "https://www.google.com/maps/dir//Kesarbay+Magjida,+Navapara+Rd,+Bhavnagar,+Gujarat+364001/@21.7736056,72.0671765,12z/data=!4m8!4m7!1m0!1m5!1m1!1s0x395f5a87119e43f3:0x39f779eed0000000!2m2!1d72.1495781!2d21.773626?entry=ttu&g_ep=EgoyMDI1MDYxNS4wIKXMDSoASAFQAw%3D%3D"
+    }
+};
